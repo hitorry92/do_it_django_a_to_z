b3e9518aa70800bef76286072bf8e5ac0e504a34
JungSeungIn <hitorry92@naver.com>
2023-11-30 16:01:21 +0900

    포스트를 작성할 때 텍스트로 태그 지정하기

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 
 class PostList(ListView):
@@ -40,7 +41,23 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         ):
             form.instance.author = current_user
             response = super(PostCreate, self).form_valid(form)
-            return super(PostCreate, self).form_valid(form)
+
+            tags_str = self.request.POST.get("tags_str")
+            if tags_str:
+                tags_str = tags_str.strip()
+
+                tags_str = tags_str.replace(",", ";")
+                tags_list = tags_str.split(";")
+
+                for t in tags_list:
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+
+            return response
 
         else:
             return redirect("/blog/")
