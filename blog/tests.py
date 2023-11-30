534782ccea2dab3848c31f70fa76f9f75053f223
JungSeungIn <hitorry92@naver.com>
2023-11-30 12:50:38 +0900

    내비게이션 바와 푸터 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -7,6 +7,23 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn("Blog", navbar.text)
+        self.assertIn("About Me", navbar.text)
+
+        logo_btn = navbar.find("a", text="Do It Django")
+        self.assertEqual(logo_btn.attrs["href"], "/")
+
+        home_btn = navbar.find("a", text="Home")
+        self.assertEqual(home_btn.attrs["href"], "/")
+
+        blog_btn = navbar.find("a", text="Blog")
+        self.assertEqual(blog_btn.attrs["href"], "/blog/")
+
+        about_me_btn = navbar.find("a", text="About Me")
+        self.assertEqual(about_me_btn.attrs["href"], "/about_me/")
+
     def test_post_list(self):
         # 1.1 포스트 목록 페이지 가져오기
         response = self.client.get("/blog/")
@@ -16,10 +33,11 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, "html.parser")
         self.assertEqual(soup.title.text.strip(), "Blog")
         # 1.4 네비게이션 바가 있음
-        navbar = soup.nav
+        # navbar = soup.nav
         # 1.5 Blog, About Me 라는 문구가 내비게이션 바에 있음
-        self.assertIn("Blog", navbar.text)
-        self.assertIn("About Me", navbar.text)
+        # self.assertIn('Blog', navbar.text)
+        # self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         # 2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -48,31 +66,32 @@ class TestView(TestCase):
         # 3.4 '아직 게시물이 없습니다'라는 문구가 더 이상 보이지 않음
         self.assertNotIn("아직 게시물이 없습니다", main_area.text)
 
-        def test_post_detail(self):
-            # 1.1. 포스트가 하나 있음
-            post_001 = Post.objects.create(
-                title="첫번째 포스트입니다.",
-                content="Hello World. We are the world.",
-            )
-            # 1.2. 그 포스트의 url 은 '/blog/1'
-            self.assertEqual(post_001.get_absolute_url(), "/blog/1/")
+    def test_post_detail(self):
+        # 1.1. 포스트가 하나 있음
+        post_001 = Post.objects.create(
+            title="첫번째 포스트입니다.",
+            content="Hello World. We are the world.",
+        )
+        # 1.2. 그 포스트의 url 은 '/blog/1'
+        self.assertEqual(post_001.get_absolute_url(), "/blog/1/")
 
-            # 2.   첫 번째 포스트의 상세 페이지 테스트
-            # 2.1. 첫 번째 포스트의 url 로 접근하면 정상적으로 작동(status code: 200)
-            response = self.client.get(post_001.get_absolute_url())
-            self.assertEqual(response.status_code, 200)
-            soup = BeautifulSoup(response.content, "html.parser")
-            # 2.2. 포스트 목록 페이지와 똑같은 네비게이션 바가 있음
-            navbar = soup.nav
-            self.assertIn("Blog", navbar.text)
-            self.assertIn("About Me", navbar.text)
-            # 2.3. 첫 번째 포스트의 제목이 브라우저 탭 타이틀에 들어 있음
-            self.assertIn(post_001.title, soup.title.text)
-            # 2.4. 첫 번째 포스트의 제목이 포스트 영역에 있음
-            main_area = soup.find("div", id="main-area")
-            post_area = main_area.find("div", id="post-area")
-            self.assertIn(post_001.title, post_area.text)
-            # 2.5. 첫 번째 포스트의 작성자(author)가 포스트 영역에 있음
-            # 아직 작성 불가
-            # 2.6. 첫 번째 포스트의 내용(content)이 포스트 영역에 있음
-            self.assertIn(post_001.content, post_area.text)
+        # 2.   첫 번째 포스트의 상세 페이지 테스트
+        # 2.1. 첫 번째 포스트의 url 로 접근하면 정상적으로 작동(status code: 200)
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, "html.parser")
+        # 2.2. 포스트 목록 페이지와 똑같은 네비게이션 바가 있음
+        # navbar = soup.nav
+        # self.assertIn('Blog', navbar.text)
+        # self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
+        # 2.3. 첫 번째 포스트의 제목이 브라우저 탭 타이틀에 들어 있음
+        self.assertIn(post_001.title, soup.title.text)
+        # 2.4. 첫 번째 포스트의 제목이 포스트 영역에 있음
+        main_area = soup.find("div", id="main-area")
+        post_area = main_area.find("div", id="post-area")
+        self.assertIn(post_001.title, post_area.text)
+        # 2.5. 첫 번째 포스트의 작성자(author)가 포스트 영역에 있음
+        # 아직 작성 불가
+        # 2.6. 첫 번째 포스트의 내용(content)이 포스트 영역에 있음
+        self.assertIn(post_001.content, post_area.text)
